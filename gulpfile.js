eae0a30df614841222a2e9953900f305bb4e7d8f
Dan Sensecall <dan@sensecall.co.uk>
2019-03-20 15:56:07 +0000

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp  = require('gulp');
+
+/**
+  Our gulp tasks live in their own files,
+  for the sake of clarity.
+ */
+require('require-dir')('./gulp-tasks');
+
+/*
+  Watch folders for changess
+*/
+gulp.task("watch", function() {
+  gulp.watch('./assets/scss/**/*.scss', gulp.parallel('css'));
+  gulp.watch('./assets/js/**/*.js', gulp.parallel('js'));
+});
+
+
+/*
+  Let's build this sucker.
+*/
+gulp.task('build', gulp.parallel(
+  'css',
+  'js'
+));
+
+/*
+  Build and watch things during dev
+*/
+gulp.task('dev', gulp.series(
+  'build',
+  'watch'
+));
