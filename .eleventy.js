f9f080ed335481061cb7bb11a240f1e16a9f70dd
Dan Sensecall <dan@sensecall.co.uk>
2019-03-26 21:41:08 +0000

    config

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,25 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addFilter("makeUppercase", function(value) {
 		return `<h1>${this.makeUppercase(name)}</h1>`;
 	});
+
+	return {
+		templateFormats: ["md", "njk", "html", "liquid", "jpg"],
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about it.
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for URLs (it does not affect your file structure)
+    pathPrefix: "/",
+
+    markdownTemplateEngine: "md",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    passthroughFileCopy: true,
+    dir: {
+    	input: ".",
+    	includes: "_includes",
+    	data: "_data",
+    	output: "_site"
+    }
+}
 };
